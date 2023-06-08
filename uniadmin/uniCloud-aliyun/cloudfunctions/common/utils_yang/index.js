700902860df66b549c71707ba66ec829a3bed9cc
“YangZhiYuan” <yangzhiyuan@memohi.com>
2023-06-08 18:42:01 +0800

    obj_yang JQL 语法

---
+++ uniadmin/uniCloud-aliyun/cloudfunctions/common/utils_yang/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	myFun:()=>{
+		return "200"
+	}
+}
--- uniadmin/uniCloud-aliyun/cloudfunctions/obj_yang/index.obj.js
+++ uniadmin/uniCloud-aliyun/cloudfunctions/obj_yang/index.obj.js
@@ -1,14 +1,30 @@
+//引入公共模块
+const {myFun} = require("utils_yang");
+
 const db = uniCloud.database()
+
+let dbJQL;
 module.exports = {
 	_before: function () { // 通用预处理器   
+	    
 	      //变量 全局可用     //函数
           this.params = this.getParams()
 		  this.startTime =Date.now()
+		  
+		  //使用jql语法
+		  dbJQL = uniCloud.databaseForJQL({
+		  	clientInfo:this.getClientInfo()
+		  })
+		  dbJQL.setUser({ 
+		  	uid: '647ea7f628064a22b1b9e97c', 
+		  	role: ['admin']
+		  })
 	},
 	async get(){
-		let res = await db.collection("yang_news").get()
+		let res = await dbJQL.collection("yang_news").where(`article_status == ${1}`)
+		.field("avatar.path as path,article_status").get()
 		let result = {
-			code:200,
+			code:myFun(),
 			msg:"ok",
 			data:res.data
 		}
@@ -25,7 +41,7 @@ module.exports = {
 	},
 	_after(error,result){
 		if(error){
-			throw err //抛出错误
+			throw error //抛出错误
 		}
 		result.timeCost =Date.now() - this.startTime
 		return result
