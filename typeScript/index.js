538fdccb55e0b64b7ac99e7fb42ef95da7e95b31
“YangZhiYuan” <yangzhiyuan@memohi.com>
2023-07-05 20:39:59 +0800

    localStorage方法

--- typeScript/index.js
+++ typeScript/index.js
@@ -1,2 +1,54 @@
 var a = new Date().toLocaleString();
-console.log(a);
+var out;
+(function (out) {
+    out[out["yes"] = 0] = "yes";
+    out[out["no"] = 1] = "no";
+})(out || (out = {}));
+var Storages = /** @class */ (function () {
+    function Storages() {
+    }
+    Storages.prototype.get = function (key) {
+        if (key) {
+            var keys = localStorage.getItem(key);
+            console.log(JSON.parse(keys));
+            return keys;
+        }
+    };
+    Storages.prototype.set = function (key, value, time) {
+        if (value != undefined && value != null) {
+            var va = {
+                value: value,
+                time: time
+            };
+            if (time != 0) {
+                localStorage.setItem('key', JSON.stringify(va));
+                return {
+                    message: "\u8FC7\u671F\u8BBE\u7F6E",
+                    value: value
+                };
+            }
+            else if (time == 0) {
+                localStorage.setItem('key', JSON.stringify(va));
+                return {
+                    message: "\u6C38\u4E0D\u8FC7\u671F\u8BBE\u7F6E",
+                    value: value
+                };
+            }
+        }
+        else {
+            return {
+                message: "\u8BBE\u7F6E".concat(key, "\u9519\u8BEF\uFF0C\u662F\u4E2A\u7A7A\u503C\u6216\u6216\u6CA1\u58F0\u660E\u7684\u503C")
+            };
+        }
+    };
+    Storages.prototype.remove = function (key) {
+        localStorage.removeItem(key);
+    };
+    Storages.prototype.clear = function () {
+        localStorage.clear();
+    };
+    return Storages;
+}());
+var times = new Storages();
+console.log(times.set('start', 123, 21));
+console.log(times.get('start'));
--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,2 +1,68 @@
 let a = new Date().toLocaleString()
-console.log(a);
+
+interface time{
+   outtme:string
+}
+
+
+type Key=string
+
+interface Result { //返回值类型
+  message: string,
+  value: object
+}
+
+interface setvalue{
+  set:<T>(key:Key,value:T,time?:number)=>void
+  get:<T>(key:Key)=>void
+  remove:(key:Key)=>void
+  clear:()=>void
+}
+enum out{
+  yes,
+  no
+}
+class Storages implements setvalue{
+   get(key:Key){
+      if(key){
+         let keys= localStorage.getItem(key) as string
+         console.log(JSON.parse(keys));
+         
+         return keys
+      }
+   } 
+   set(key:Key,value:any,time?:out|number){
+      if(value!=undefined && value!=null ){
+         let va ={
+          value,
+          time
+         }
+         if(time!=0){
+            localStorage.setItem('key',JSON.stringify(va))
+            return{
+              message:`过期设置`,
+              value:value
+            }
+         }else if(time==0){
+            localStorage.setItem('key',JSON.stringify(va))
+            return{
+              message:`永不过期设置`,
+              value:value
+            }
+         }
+      }else{
+         return {
+           message:`设置${key}错误，是个空值或或没声明的值`
+         }
+      }
+   }
+   remove(key:Key){
+     localStorage.removeItem(key)
+   }
+   clear(){
+     localStorage.clear()
+   }
+}
+const times =new Storages()
+console.log(times.set('start',123,21));
+console.log(times.get('start'));
