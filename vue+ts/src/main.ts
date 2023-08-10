04b4e8d009e95f83dcdfdca5cb428f19a4828c93
“YangZhiYuan” <yangzhiyuan@memohi.com>
2023-08-10 10:45:25 +0800

    add branch develop

--- vue+ts/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-import router from "./router/index";
-createApp(App).use(pinia).use(router).mount('#app')
