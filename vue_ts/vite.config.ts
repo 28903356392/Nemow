6666a76aa4785d069cc1296c1e07cfcc39e4865f
“YangZhiYuan” <yangzhiyuan@memohi.com>
2023-08-28 14:29:41 +0800

    feat(vue工程化项目模板): 工程化项目模板

--- vue_ts/vite.config.ts
+++ vue_ts/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+
+// JSX reate开发的语法
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue(), eslintPlugin(), vueJsx({})],
 })
